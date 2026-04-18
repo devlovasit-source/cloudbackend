cfc47627622ac0c038512b9d5da4e0e515ba7fc0
devlovasit-source <devlovasit@gmail.com>
2026-04-18 19:35:28 +0530

    Update style_board_renderer.py

--- brain/engines/style_board_renderer.py
+++ brain/engines/style_board_renderer.py
@@ -5,7 +5,7 @@ from typing import Dict, Any
 import requests
 from PIL import Image, ImageFilter, ImageDraw, ImageFont
 
-from routers.remove_bg import remove_background_sync
+from routers.bg_remover import remove_background_sync
 
 
 class StyleBoardRenderer:
